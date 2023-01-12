b28996e030ed31953996e33853076dc18958b6ea
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-01-12 17:31:26 +0000

    Deploying to gh-pages from @ Lukyianenko/goit-js-hw-10@cc119bbd43a819b86d8c23350e7bbad5fb6d4e34 🚀

---
+++ index.12a19f46.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/spain").then((e=>console.log(e)));
+//# sourceMappingURL=index.12a19f46.js.map
---
+++ index.c41e1748.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/spain").then((function(n){return console.log(n)}));
+//# sourceMappingURL=index.c41e1748.js.map
